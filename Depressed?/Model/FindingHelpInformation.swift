a24ea52a4bbdade8197aaaa2d1cca4c4aa3db852
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-09-16 18:48:48 -0400

    Update FindingHelpInformation.swift

--- Depressed?/Model/FindingHelpInformation.swift
+++ Depressed?/Model/FindingHelpInformation.swift
@@ -14,7 +14,7 @@ public struct FindingHelpInformation {
     }
 
     ///  Creates a new `FindingHelpInformation` if information is available
-    ///  for the given
+    ///  for the giv
     ///
     ///  - parameter locale: The locale for which the information is suitable.
     ///
