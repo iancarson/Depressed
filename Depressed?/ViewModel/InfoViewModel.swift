06304813b0838472698380725712357438e7f9ef
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-09-19 19:42:14 -0400

    Update InfoViewModel.swift

--- Depressed?/ViewModel/InfoViewModel.swift
+++ Depressed?/ViewModel/InfoViewModel.swift
@@ -2,7 +2,7 @@ import Foundation
 
 public struct InfoViewModel {
 
-    /// The current version and build number as a user-pres
+    /// The current version and build number as a user
     public let versionInformation: String = {
 
         if let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String,
