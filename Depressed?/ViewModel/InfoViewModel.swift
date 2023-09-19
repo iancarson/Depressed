b37cf5b1b512b3ce7ef6f38b51e0beba0e8162c5
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-09-19 19:37:58 -0400

    Update InfoViewModel.swift

--- Depressed?/ViewModel/InfoViewModel.swift
+++ Depressed?/ViewModel/InfoViewModel.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-///  Viewmodel for displaying 'About' informat
+///  Viewmodel for displaying 'About' infor
 public struct InfoViewModel {
 
     /// The current version and build number as a user-presentable string.
