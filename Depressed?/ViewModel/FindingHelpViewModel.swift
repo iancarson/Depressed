74ec051ef6f3985bc3cbe493a6b2ece7f9438baa
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-09-20 19:26:37 -0400

    Update FindingHelpViewModel.swift

--- Depressed?/ViewModel/FindingHelpViewModel.swift
+++ Depressed?/ViewModel/FindingHelpViewModel.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-///  Viewmodel for displaying information of 
+///  Viewmodel for displaying information 
 public struct FindingHelpViewModel {
 
     /// The URL of the help offer.
