3dcb37b5c73cfe99cfaf391c644319c29827537f
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-09-21 19:31:25 -0400

    Update EvaluationViewModel.swift

--- Depressed?/ViewModel/EvaluationViewModel.swift
+++ Depressed?/ViewModel/EvaluationViewModel.swift
@@ -20,7 +20,7 @@ public struct EvaluationViewModel {
     /// The titles of the questions the user has answered and the corresponding score.
     public let answers: [(String, String)]
 
-    /// The viewm
+    /// The vi
     public let findingHelpViewModel: FindingHelpViewModel?
 
     /// Whether or not to display the 'Find Help' information.
