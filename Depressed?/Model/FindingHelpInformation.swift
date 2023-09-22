0df68d85379f6837beef1a9872fab65d656ac8ed
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-09-22 19:58:38 -0400

    Update FindingHelpInformation.swift

--- Depressed?/Model/FindingHelpInformation.swift
+++ Depressed?/Model/FindingHelpInformation.swift
@@ -10,7 +10,7 @@ public struct FindingHelpInformation {
         self.url = url
         self.organizationName = organizationName
     }
-    ///  - parameter locale: The locale f
+    ///  - parameter locale: The locale
     ///
     ///  - returns: A newly initialized `FindingHelpInformation` or `nil`.
     public init?(locale: Locale) {
