b6d712bdefdb44d5750e903fb20270dde9e167f8
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-09-23 19:21:00 -0400

    Update FindingHelpViewModel.swift

--- Depressed?/ViewModel/FindingHelpViewModel.swift
+++ Depressed?/ViewModel/FindingHelpViewModel.swift
@@ -10,7 +10,7 @@ public struct FindingHelpViewModel {
 
     ///  Creates a new `FindingHelpViewModel` for the given information
     ///
-    ///  - parameter info: A `FindingHelpIn
+    ///  - parameter info: A `FindingHelp
     ///  - returns: A newly initialized `FindingHelpViewModel` instance
     public init(info: FindingHelpInformation) {
         url = info.url as URL
