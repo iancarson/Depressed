4028fccb916094783b584aa4db127270e209dca7
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-09-26 20:45:38 -0400

    Update Question.swift

--- Depressed?/Model/Question.swift
+++ Depressed?/Model/Question.swift
@@ -16,7 +16,7 @@ public struct Question {
     }
 
     ///  Creates a new `Question` from a `QuestionIdentifier`.
-    ///  - parameter identifier: a question identifi
+    ///  - parameter identifier: a question identif
     ///  - returns: A newly initialized Question.
     public init(identifier: QuestionIdentifier) {
 
