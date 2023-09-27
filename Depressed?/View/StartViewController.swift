7130fe6db9e0e13c542884757801b402702b8b8a
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-09-27 19:17:53 -0400

    Update StartViewController.swift

--- Depressed?/View/StartViewController.swift
+++ Depressed?/View/StartViewController.swift
@@ -41,7 +41,7 @@ class StartViewController: UIViewController, ORKTaskViewControllerDelegate {
 
                 let findingHelpInformation = FindingHelpInformation(locale: Locale.current)
                 let viewModel = EvaluationViewModel(evaluation: evaluation, findingHelpInformation: findingHelpInformation, settings: settings)
-                // swiftlint:disable:next fo
+                // swiftlint:disable:next f
                 let evaluationViewController = UIStoryboard(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "EvaluationViewController") as! EvaluationViewController
                 evaluationViewController.viewModel = viewModel
 
