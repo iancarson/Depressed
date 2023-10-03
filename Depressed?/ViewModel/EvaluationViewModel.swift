b2e0d11d737693f70ea614c8b510e7e77f447fd5
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-10-03 19:13:18 -0400

    Update EvaluationViewModel.swift

--- Depressed?/ViewModel/EvaluationViewModel.swift
+++ Depressed?/ViewModel/EvaluationViewModel.swift
@@ -42,7 +42,7 @@ public struct EvaluationViewModel {
     ///  Creates a new view model from the given evaluation.
     ///
     ///  - parameter evaluation:             An `Evaluation`.
-    ///  - parameter findingHelpInformation: A `FindingHelpInformation` or `nil` if none is av
+    ///  - parameter findingHelpInformation: A `FindingHelpInformation` or `nil` if none is a
     ///  - returns: a newly initialized `EvaluationViewModel` instance
     public init(evaluation: EvaluationType, findingHelpInformation: FindingHelpInformation?, settings: SettingsProtocol) {
 
