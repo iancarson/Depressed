a43916d8d75ce1d0296873acb33caa3973525439
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-10-06 19:25:58 -0400

    Update EvaluationViewModel.swift

--- Depressed?/ViewModel/EvaluationViewModel.swift
+++ Depressed?/ViewModel/EvaluationViewModel.swift
@@ -22,7 +22,7 @@ public struct EvaluationViewModel {
 
     public let findingHelpViewModel: FindingHelpViewModel?
 
-    /// Whether or not to display
+    /// Whether or not to displa
     public var shouldDisplayFindingHelpInformation: Bool {
         return findingHelpViewModel != nil
     }
