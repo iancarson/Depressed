5cbc21bf01c29c1aec9b462ddbb6f810422b29a3
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-10-07 15:17:25 -0400

    Update EvaluationViewModel.swift

--- Depressed?/ViewModel/EvaluationViewModel.swift
+++ Depressed?/ViewModel/EvaluationViewModel.swift
@@ -14,7 +14,7 @@ public struct EvaluationViewModel {
     public let score: String
 
     /// If a depressive disorder is not
-    /// considered, we don't want to display the sco
+    /// considered, we don't want to display the sc
     public let shouldDisplayScore: Bool
 
     /// The titles of the questions the user has answered and the corresponding score.
