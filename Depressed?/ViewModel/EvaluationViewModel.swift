b71b64ec43f4c2cb343949966e502ac2fe521f4b
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-10-08 10:04:54 -0400

    Update EvaluationViewModel.swift

--- Depressed?/ViewModel/EvaluationViewModel.swift
+++ Depressed?/ViewModel/EvaluationViewModel.swift
@@ -25,7 +25,7 @@ public struct EvaluationViewModel {
         return findingHelpViewModel != nil
     }
 
-    /// Whether or not the user should be pr
+    /// Whether or not the user should be p
     public var shouldPromptForReview: Bool {
 
         if ProcessInfo.processInfo.environment["UITests"]?.isEmpty == false {
