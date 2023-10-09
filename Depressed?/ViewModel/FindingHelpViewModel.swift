596f6619c5b304f3774e7f6cbac3538d88d97981
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-10-09 14:16:49 -0400

    Update FindingHelpViewModel.swift

--- Depressed?/ViewModel/FindingHelpViewModel.swift
+++ Depressed?/ViewModel/FindingHelpViewModel.swift
@@ -2,7 +2,7 @@ import Foundation
 
 public struct FindingHelpViewModel {
 
-    /// The URL of the help offe
+    /// The URL of the help off
     public let url: URL
 
     /// A string describing the source of the help offer.
