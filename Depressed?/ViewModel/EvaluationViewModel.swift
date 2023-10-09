a503731877a2e866d945462e81e1335559c83586
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-10-09 14:04:19 -0400

    Update EvaluationViewModel.swift

--- Depressed?/ViewModel/EvaluationViewModel.swift
+++ Depressed?/ViewModel/EvaluationViewModel.swift
@@ -13,7 +13,7 @@ public struct EvaluationViewModel {
 
     public let score: String
 
-    /// If a depressive dis
+    /// If a depressive di
     public let shouldDisplayScore: Bool
 
     /// The titles of the questions the user has answered and the corresponding score.
