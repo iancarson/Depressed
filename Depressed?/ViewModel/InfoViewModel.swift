c27a6e508ee44e7b23e75a9763463d2cca1f6844
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-10-10 19:19:00 -0400

    Update InfoViewModel.swift

--- Depressed?/ViewModel/InfoViewModel.swift
+++ Depressed?/ViewModel/InfoViewModel.swift
@@ -13,7 +13,7 @@ public struct InfoViewModel {
     }()
 
     ///  Creates a new `InfoViewModel`.
-    ///  - returns: a new `InfoViewModel` in
+    ///  - returns: a new `InfoViewModel` i
     public init() {}
 
 }
