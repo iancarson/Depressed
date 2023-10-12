235ea04b641a743cc7d55c6e1e2bee4f6c649681
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-10-12 19:43:18 -0400

    Update Answer.swift

--- Depressed?/Model/Answer.swift
+++ Depressed?/Model/Answer.swift
@@ -2,7 +2,7 @@ import ResearchKit
 
 public struct Answer {
 
-    /// The answered que
+    /// The answered qu
     public let question: Question
     public let answerScore: PHQ9ChoiceValue
     
