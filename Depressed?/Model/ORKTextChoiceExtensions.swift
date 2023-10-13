d588930cf5095f2af594f6c310fc56d554daafd3
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-10-13 16:14:36 -0400

    Update ORKTextChoiceExtensions.swift

--- Depressed?/Model/ORKTextChoiceExtensions.swift
+++ Depressed?/Model/ORKTextChoiceExtensions.swift
@@ -1,7 +1,7 @@
 import ResearchKit
 
 ///  Answer choice and the associated value.
-///  The values are used to calculate the overall scor
+///  The values are used to calculate the overall sco
 public enum PHQ9ChoiceValue: Int {
 
     case notAtAll = 0
