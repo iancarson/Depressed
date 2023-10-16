32e2356ed1d8df19a0c72078d1a113ed189da763
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-10-16 19:25:21 -0400

    Update Question.swift

--- Depressed?/Model/Question.swift
+++ Depressed?/Model/Question.swift
@@ -5,7 +5,7 @@ public struct Question {
     public let title: String
     public let text: String
 
-    /// An `ORKStep` repre
+    /// An `ORKStep` repr
     public var step: ORKQuestionStep {
         let step = ORKQuestionStep(identifier: identifier.rawValue,
                                    title: title,
