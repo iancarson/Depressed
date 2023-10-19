ac7d084ddabc8f3c80792a219631bb38e94b4fff
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-10-18 20:29:27 -0400

    Update SelfTestTask.swift

--- Depressed?/Model/SelfTestTask.swift
+++ Depressed?/Model/SelfTestTask.swift
@@ -1,6 +1,6 @@
 import ResearchKit
 
-/// Task that contains the questions of the [
+/// Task that contains the questions of the 
 open class SelfTestTask: ORKOrderedTask {
     
     public class func task() -> SelfTestTask {
