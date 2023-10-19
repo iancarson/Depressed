a90f508cb254982da676fe11e89afd6515bc42b9
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-10-18 20:20:51 -0400

    Update SelfTestTask.swift

--- Depressed?/Model/SelfTestTask.swift
+++ Depressed?/Model/SelfTestTask.swift
@@ -1,6 +1,6 @@
 import ResearchKit
 
-/// Task that contains the questions of the [PHQ-9](https://en.wikipedia.org/wiki/Patient_Health_Questionna
+/// Task that contains the questions of the [PHQ-9](https://en.wikipedia.org/wiki/Patient_Health_Questionn
 open class SelfTestTask: ORKOrderedTask {
     
     public class func task() -> SelfTestTask {
