67fdb0faaf6f30e969b233f73890567ada0ced8d
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-10-19 20:50:22 -0400

    Update SelfTestTask.swift

--- Depressed?/Model/SelfTestTask.swift
+++ Depressed?/Model/SelfTestTask.swift
@@ -18,7 +18,7 @@ open class SelfTestTask: ORKOrderedTask {
         return SelfTestTask(identifier: "SelfTest", steps: steps)
     }
     
-    ///  - returns: The progress of the c
+    ///  - returns: The progress of the 
     open override func progress(ofCurrentStep step: ORKStep, with result: ORKTaskResult) -> ORKTaskProgress {
         let index = steps.index(of: step)
 
