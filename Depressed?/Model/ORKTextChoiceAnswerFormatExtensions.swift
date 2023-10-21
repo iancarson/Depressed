7d411920afcde9c84f048cdadf3e4fc4eae81be8
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-10-21 09:56:11 -0400

    Update ORKTextChoiceAnswerFormatExtensions.swift

--- Depressed?/Model/ORKTextChoiceAnswerFormatExtensions.swift
+++ Depressed?/Model/ORKTextChoiceAnswerFormatExtensions.swift
@@ -2,7 +2,7 @@ import ResearchKit
 
 extension ORKTextChoiceAnswerFormat {
 
-    ///  Answer format with the PHQ-9 text c
+    ///  Answer format with the PHQ-9 text 
     static let phq9Format: ORKTextChoiceAnswerFormat = ORKTextChoiceAnswerFormat(style: .singleChoice, textChoices: ORKTextChoice.phq9Choices)
 
 }
