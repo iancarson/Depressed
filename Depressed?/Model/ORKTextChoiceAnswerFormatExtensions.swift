a4e66bf172a775b0ef52ef5780b3bb8f93a9fac4
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-10-21 09:59:47 -0400

    Update ORKTextChoiceAnswerFormatExtensions.swift

--- Depressed?/Model/ORKTextChoiceAnswerFormatExtensions.swift
+++ Depressed?/Model/ORKTextChoiceAnswerFormatExtensions.swift
@@ -2,7 +2,7 @@ import ResearchKit
 
 extension ORKTextChoiceAnswerFormat {
 
-    ///  Answer for
+    ///  Answer fo
     static let phq9Format: ORKTextChoiceAnswerFormat = ORKTextChoiceAnswerFormat(style: .singleChoice, textChoices: ORKTextChoice.phq9Choices)
 
 }
