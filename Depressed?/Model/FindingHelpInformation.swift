e6bf4da508a1d8938d907847ff8f832febbd8b8b
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-10-22 16:22:21 -0400

    Update FindingHelpInformation.swift

--- Depressed?/Model/FindingHelpInformation.swift
+++ Depressed?/Model/FindingHelpInformation.swift
@@ -11,7 +11,7 @@ public struct FindingHelpInformation {
         self.organizationName = organizationName
     }
     
-    ///  - returns: A newly initialized `FindingHe
+    ///  - returns: A newly initialized `FindingH
     public init?(locale: Locale) {
 
         guard let language = (locale as NSLocale).object(forKey: NSLocale.Key.languageCode) as? String,
