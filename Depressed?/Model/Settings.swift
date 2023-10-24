b082012b4efe9fd19cc5574b8f302eba60da086b
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-10-24 19:33:18 -0400

    Update Settings.swift

--- Depressed?/Model/Settings.swift
+++ Depressed?/Model/Settings.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-/// Types conforming to this protocol can store and retrieve local settin
+/// Types conforming to this protocol can store and retrieve local setti
 public protocol SettingsProtocol: class {
     var numberOfFinishedSurveys: Int { get set }
  
