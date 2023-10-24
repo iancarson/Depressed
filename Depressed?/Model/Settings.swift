96d08e31c673ccc52ec34743f97cdb5dfb4e1b30
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-10-24 19:34:49 -0400

    Update Settings.swift

--- Depressed?/Model/Settings.swift
+++ Depressed?/Model/Settings.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-/// Types conforming to this protocol can store and retrie
+/// Types conforming to this protocol can store and retri
 public protocol SettingsProtocol: class {
     var numberOfFinishedSurveys: Int { get set }
  
