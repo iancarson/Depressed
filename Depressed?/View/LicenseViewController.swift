630f1c234da35a7d8ab6f2b0111d886a24f42f64
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-10-26 16:00:06 -0400

    Update LicenseViewController.swift

--- Depressed?/View/LicenseViewController.swift
+++ Depressed?/View/LicenseViewController.swift
@@ -7,7 +7,7 @@ class LicenseViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // but on iOS >= 9 the layout is borked unless we s
+        // but on iOS >= 9 the layout is borked unless we 
         let license = textView.text
         textView.text = ""
         DispatchQueue.main.async {
