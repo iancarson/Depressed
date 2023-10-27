05b0d1c6d670b9fae3e1ec9d2a2d5bddb1e00613
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-10-27 13:23:56 -0400

    Update LicenseViewController.swift

--- Depressed?/View/LicenseViewController.swift
+++ Depressed?/View/LicenseViewController.swift
@@ -7,7 +7,7 @@ class LicenseViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        //Set Version agai
+        //Set Version again
         let license = textView.text
         textView.text = ""
         DispatchQueue.main.async {
