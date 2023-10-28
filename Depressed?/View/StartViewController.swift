21505da1eb04d9935cba6eab5c2ba4d63425ee6a
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-10-28 15:31:10 -0400

    Update StartViewController.swift

--- Depressed?/View/StartViewController.swift
+++ Depressed?/View/StartViewController.swift
@@ -26,7 +26,7 @@ class StartViewController: UIViewController, ORKTaskViewControllerDelegate {
         present(taskController, animated: true, completion: nil)
     }
 
-    // MARK: - ORKTa
+    // MARK: - ORKT
     func taskViewController(_ taskViewController: ORKTaskViewController, didFinishWith reason: ORKTaskViewControllerFinishReason, error: Error?) {
 
         if reason == .completed, let results = taskViewController.result.results as? [ORKStepResult] {
