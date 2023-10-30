b27e43c1a5ef6ebbd887c3b69848e95c19e3f253
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-10-30 19:50:49 -0400

    Update EvaluationViewModel.swift

--- Depressed?/ViewModel/EvaluationViewModel.swift
+++ Depressed?/ViewModel/EvaluationViewModel.swift
@@ -34,7 +34,7 @@ public struct EvaluationViewModel {
 
     private let settings: SettingsProtocol
 
-    ///  - returns: a newly initialized `Evaluation
+    ///  - returns: a newly initialized `Evaluatio
     public init(evaluation: EvaluationType, findingHelpInformation: FindingHelpInformation?, settings: SettingsProtocol) {
 
         self.settings = settings
