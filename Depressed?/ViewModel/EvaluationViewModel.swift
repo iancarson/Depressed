b1389fd57bb4737af8c7ecbadde3ef9f42c92634
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-11-01 11:37:16 -0400

    Update EvaluationViewModel.swift

--- Depressed?/ViewModel/EvaluationViewModel.swift
+++ Depressed?/ViewModel/EvaluationViewModel.swift
@@ -92,7 +92,7 @@ public struct EvaluationViewModel {
         answers = evaluation.answers.map { ($0.question.title, String($0.answerScore.rawValue)) }
     }
 
-    /// Called when the viewController asked the system to
+    /// Called when the viewController asked the system t
     public func didShowReviewPrompt() {
         settings.didShowRatingPrompt = true
     }
