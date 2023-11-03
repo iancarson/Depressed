7f7391339bd8f7147d8b984fa810cd4295c14698
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-11-02 20:22:42 -0400

    Update EvaluationViewModel.swift

--- Depressed?/ViewModel/EvaluationViewModel.swift
+++ Depressed?/ViewModel/EvaluationViewModel.swift
@@ -14,7 +14,7 @@ public struct EvaluationViewModel {
 
     public let shouldDisplayScore: Bool
 
-    /// The titles of the questions the user has answere
+    /// The titles of the questions the user has answer
     public let answers: [(String, String)]
 
     public let findingHelpViewModel: FindingHelpViewModel?
