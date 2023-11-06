2301a839814a11e3a93720d176612fcce42c24ba
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-11-06 10:30:49 -0500

    Update EvaluationViewModel.swift

--- Depressed?/ViewModel/EvaluationViewModel.swift
+++ Depressed?/ViewModel/EvaluationViewModel.swift
@@ -7,7 +7,7 @@ public struct EvaluationViewModel {
     public let diagnosisText: String
     public let explanationText: String
 
-    /// Warning message i
+    /// Warning message 
     public let suicidalText: String?
 
     public let score: String
