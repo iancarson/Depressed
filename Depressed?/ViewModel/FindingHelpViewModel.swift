752ce22a41bf6c090cf8c2c4849c44042d9b69e7
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-11-06 10:35:13 -0500

    Update FindingHelpViewModel.swift

--- Depressed?/ViewModel/FindingHelpViewModel.swift
+++ Depressed?/ViewModel/FindingHelpViewModel.swift
@@ -4,7 +4,7 @@ public struct FindingHelpViewModel {
 
     public let url: URL
 
-    /// A string describing the source of the he
+    /// A string describing the source of the h
     public let credits: String
 
     ///  Creates a new `FindingHelpViewModel` for the given information
