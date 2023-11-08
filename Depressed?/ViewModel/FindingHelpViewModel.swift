ec58a5d3ba722a8e7b91d6abc8e6a93afab050d4
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-11-08 15:34:54 -0500

    Update FindingHelpViewModel.swift

--- Depressed?/ViewModel/FindingHelpViewModel.swift
+++ Depressed?/ViewModel/FindingHelpViewModel.swift
@@ -6,7 +6,7 @@ public struct FindingHelpViewModel {
 
     public let credits: String
 
-    ///  Creates a new `FindingHelpViewModel` for the given inf
+    ///  Creates a new `FindingHelpViewModel` for the given in
     public init(info: FindingHelpInformation) {
         url = info.url as URL
         credits = String(format: NSLocalizedString("find_help_credits_format", comment: ""), info.organizationName)
