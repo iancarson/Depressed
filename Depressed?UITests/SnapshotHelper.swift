d7eb368711823e8196e087255b960dc1cb7c829c
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-11-15 14:34:02 -0500

    Update SnapshotHelper.swift

--- Depressed?UITests/SnapshotHelper.swift
+++ Depressed?UITests/SnapshotHelper.swift
@@ -24,7 +24,7 @@ func snapshot(_ name: String, waitForLoadingIndicator: Bool) {
     }
 }
 
-/// - Parameter
+/// - Paramete
 ///   - timeout: Amount of seconds to wait until the network loading indicator disappears. Pass `0` if you don't want to wait.
 func snapshot(_ name: String, timeWaitingForIdle timeout: TimeInterval = 20) {
     Snapshot.snapshot(name, timeWaitingForIdle: timeout)
