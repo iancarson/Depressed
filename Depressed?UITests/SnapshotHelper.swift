ddc1ea0bce8e06e715cbe4df46e925eb2d3c7ead
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-11-20 13:03:58 -0500

    Update SnapshotHelper.swift
    Refactor the Comments accordingly

--- Depressed?UITests/SnapshotHelper.swift
+++ Depressed?UITests/SnapshotHelper.swift
@@ -143,7 +143,7 @@ open class Snapshot: NSObject {
             waitForLoadingIndicatorToDisappear(within: timeout)
         }
 
-        print("snapshot: \(name)") // more information about thcheck out https://docs.fastlane.tools/actions/snapshot/#how-does-it-work
+        print("snapshot: \(name)") // more information about tcheck out https://docs.fastlane.tools/actions/snapshot/#how-does-it-work
 
         sleep(1) 
 
