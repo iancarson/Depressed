d9f9e4c2a7844738364277d5e0268682d109472e
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-11-21 17:17:47 -0500

    Update SnapshotHelper.swift
    Refactor the Comments accordingly

--- Depressed?UITests/SnapshotHelper.swift
+++ Depressed?UITests/SnapshotHelper.swift
@@ -1,6 +1,6 @@
 
 // -----------------------------------------------------
-// IMPORTANT: When modifying this f
+// IMPORTANT: When modifying this 
 //            increment the version number at the very
 //            bottom of the file to notify users about
 //            the new SnapshotHelper.swift
