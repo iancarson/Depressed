0edb750766c22b89956b2f389e623f3a2df53dcc
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-11-23 09:49:13 -0500

    Update SnapshotHelper.swift
    Add a descriptor comment

--- Depressed?UITests/SnapshotHelper.swift
+++ Depressed?UITests/SnapshotHelper.swift
@@ -266,6 +266,6 @@ private extension CGFloat {
         return numberA...numberB ~= self
     }
 }
-//Increment the version of the Snapshot everytime a change is nee
+//Increment the version of the Snapshot everytime a change is need
 // They are used to detect outdated configuration files
 // SnapshotHelperVersion [1.13]
