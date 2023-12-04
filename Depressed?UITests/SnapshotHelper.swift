3e91642d5ff64d4b3c207290b91c7cb713968633
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-12-04 12:58:23 -0500

    Update SnapshotHelper.swift
    Refactor the comments appropriately

--- Depressed?UITests/SnapshotHelper.swift
+++ Depressed?UITests/SnapshotHelper.swift
@@ -17,7 +17,7 @@ func snapshot(_ name: String, waitForLoadingIndicator: Bool) {
     }
 }
 
-//Pass `0` if you don't want 
+//Pass `0` if you don't want
 func snapshot(_ name: String, timeWaitingForIdle timeout: TimeInterval = 20) {
     Snapshot.snapshot(name, timeWaitingForIdle: timeout)
 }
