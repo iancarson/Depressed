e57609ca97eb15cea03e3d24bf7d38a2225d1669
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-12-17 22:10:22 -0500

    Update SnapshotHelper.swift
    Refactor the comments to enhance comprehension

--- Depressed?UITests/SnapshotHelper.swift
+++ Depressed?UITests/SnapshotHelper.swift
@@ -258,6 +258,6 @@ private extension CGFloat {
         return numberA...numberB ~= self
     }
 }
-//Increase the
+//Increase the 
 // They are used to detect outdated version
 // SnapshotHelperVersion [1.13]
